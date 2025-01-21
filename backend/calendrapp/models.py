f87b2fc6315589343bc807d7470b6d76be1f6188
Will Schrant <wschrant@gmail.com>
2025-01-20 20:17:07 -0500

    Started work on the database side. Still some parts I want to look over, especially what other tables I want to add and modifying the group model. Also gotta connect/create the DB in PostgreSQL

--- backend/calendrapp/models.py
+++ backend/calendrapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=40, unique=True)
+    password = models.CharField(max_length=100)
+    type = models.CharField(max_length=40)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+class GroupMember(models.Model):
+    rank = models.CharField(max_length=100)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+class ToDoPost(models.Model):
+    title = models.CharField(max_length=255) 
+    content = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
---
+++ backend/calendrapp/serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from . models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', "username", "password", "type", "created_date", "updated_date"]
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta: Group
+    fields = ["name", "created_date", "updated_date"]
+
+class GroupMemberSerializer(serializers.ModelSerializer):
+    class Meta: GroupMember
+    fields = ["rank", "group", "user", "created_date", "updated_date"]
+
+class ToDoPostSerializer(serializers.ModelSerializer):
+    class Meta: ToDoPost
+    fields = ["title", "content", "date", "time", "user", "group" "created_date", "updated_date"]
